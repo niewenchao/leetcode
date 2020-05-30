3b201ec68a146535e6f48af0da7752246c01de42
nwenchao <nwenchao@gmail.com>
2020-05-30 15:52:33 +0800

    无序符号表：基于拉链法的散列表（空间换时间的典型）

---
+++ src/main/java/st/SeparateChainingHashST.java
@@ -0,0 +1,63 @@
+package st;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 基于拉链法的散列表，N个键值对存放在M条拉链上，N>M
+ * 使用拉链解决碰撞冲突
+ * @param <K>
+ * @param <V>
+ */
+public class SeparateChainingHashST<K,V> implements ST<K,V> {
+    int N;                                                      //键值对个数
+    int M;                                                      //拉链个数
+    SequentialSearchST<K,V>[] st;
+
+    public SeparateChainingHashST(int size){
+        st = new SequentialSearchST[size];
+        for (int i = 0; i < size; i++) {
+            st[i] = new SequentialSearchST<K,V>();
+        }
+        M = size;
+    }
+
+    public SeparateChainingHashST(){
+        this(997);
+    }
+
+    private int hash(K k){
+        return (k.hashCode() & 0x7fffffff) % M;
+    }
+
+    public  void put(K k, V v){
+        if(!contains(k)) N++;
+        st[hash(k)].put(k,v);
+    }
+    public  void delete(K k){
+        if(contains(k))
+        {st[hash(k)].delete(k);N--;}
+    }
+    public  V get(K k){
+       return st[hash(k)].get(k);
+    }
+    public  int size(){
+        return N;
+    }
+
+    public  boolean isEmpty(){
+        return N == 0;
+    }
+    public  boolean contains(K k){
+        return get(k) != null;
+    }
+    public Iterable<K> keys(){
+        List<K> list = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            for(K k: st[i].keys()){
+                list.add(k);
+            }
+        }
+        return list;
+    }
+}
