4b4167c2d080df8fbe1651d75cebe0685c57108e
nwenchao <nwenchao@gmail.com>
2020-06-21 14:16:51 +0800

    /** test case **/

--- src/main/java/dp/TestCase.java
+++ src/main/java/dp/TestCase.java
@@ -2,7 +2,6 @@ package dp;
 
 public  class TestCase {
     public static void main(String[] args){
-        //System.out.println(LIS.lengthOfLIS(new int[]{1,4,3,4,2}));
         //System.out.println(Knapsack.knapsack01(4, new int[]{2,1,3}, new int[]{4,2,3} ));
         //System.out.println(Knapsack.change(4, new int[]{2,1,3}));
         //System.out.println(Knapsack.canPartition(new int[]{1,5,11,5}));
