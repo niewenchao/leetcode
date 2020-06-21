f1f0372b2dee76a5a069e8eecd300dd5d38f79e9
nwenchao <nwenchao@gmail.com>
2020-06-21 14:16:00 +0800

    /** 高楼扔鸡蛋问题 **/

--- src/main/java/dp/SuperEggDrop.java
+++ src/main/java/dp/SuperEggDrop.java
@@ -24,13 +24,32 @@ public class SuperEggDrop {
         if(N == 0) return 0;
         if(memo[K][N] < Integer.MAX_VALUE) return memo[K][N];
         int res = Integer.MAX_VALUE;
+        //穷举所有可能的选择，取每个选择最坏情况下的最小值
         for (int i = 1; i <= N; i++) {
             res = Math.min(res,Math.max(dp(K-1,i-1) + 1,dp(K, N - i) + 1));
         }
+        memo[K][N] = res;
         return res;
     }
 
+    /**
+     *  dp[k][m]:有k个鸡蛋，m次数，请问最坏情况下可以测试的楼层高度
+     *  dp[k][m] = dp[k][m-1] + dp[k-1][m-1] + 1;
+     */
+    public int superEggDrop01(int K, int N){
+        int dp[][] = new int[K+1][N+1];
+        int m = 0;
+        //当前测试次数测不了N层楼时
+        while (dp[K][m] < N){
+            m++;            //增加测试次数
+            for (int k = 1; k <= K; k++) {
+                dp[k][m] = dp[k-1][m-1] + dp[k][m-1] + 1;
+            }
+        }
+        return m;
+    }
+
     public static void main(String[] args){
-        System.out.println(new SuperEggDrop().superEggDrop(1,7));
+        System.out.println(new SuperEggDrop().superEggDrop01(2,1000));
     }
 }
